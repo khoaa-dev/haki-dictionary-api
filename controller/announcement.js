0ed57d9879ad502c08265033cff2057b2d7fd8c7
nguyendinhkhoa19t1@gmail.com <nguyendinhkhoa19t1@gmail.com>
2021-12-20 16:28:26 +0700

    create vs delete announ function

--- controller/announcement.js
+++ controller/announcement.js
@@ -12,3 +12,54 @@ export const getAllAnnouncement = (req, res) => {
         res.send(err);
     })
 }
+
+export const createAnnouncement = (req, res) => {
+    const data = {
+        title: req.body.title,
+        content: req.body.content,
+        createdTime: req.body.createdTime,
+        imageUrl: req.body.imageUrl
+    };
+    sql.connect(config).then(pool => {
+        return pool.request()
+            .query(`set dateformat dmy
+                    insert into Announcement (title, content, createdTime, imageUrl)
+                    values (N'${data.title}', N'${data.content}', '${data.createdTime}', '${data.imageUrl}')`);
+        
+    }).then(result => {
+        if(result.rowsAffected == 1) {
+            res.send({
+                "status": "Create successfully"
+            });
+        } else {
+            res.send({
+                "status": "Create failed"
+            });
+        }
+        
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+export const deleteAnnouncement = (req, res) => {
+    const id = req.body.id;
+    sql.connect(config).then(pool => {
+        return pool.request()
+            .query(`delete from Announcement where id = ${id})`);
+        
+    }).then(result => {
+        if(result.rowsAffected == 1) {
+            res.send({
+                "status": "Delete successfully"
+            });
+        } else {
+            res.send({
+                "status": "Delete failed"
+            });
+        }
+        
+    }).catch(err => {
+        console.log(err);
+    });
+}
--- routes/announcements.js
+++ routes/announcements.js
@@ -1,9 +1,13 @@
 import express from "express";
 
-import { getAllAnnouncement } from "../controller/announcement.js";
+import { getAllAnnouncement, createAnnouncement, deleteAnnouncement } from "../controller/announcement.js";
 
 const router = express.Router();
 
 router.get('/', getAllAnnouncement);
+
+router.post('/createAnnouncement', createAnnouncement);
+
+router.post('/deleteAnnouncement/:id', deleteAnnouncement);
 
 export default router;
