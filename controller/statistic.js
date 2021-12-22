c82f9a43172151fc80af1a05d53fbe081322c14a
nguyendinhkhoa19t1@gmail.com <nguyendinhkhoa19t1@gmail.com>
2021-12-22 20:36:46 +0700

    update count word Vie Eng

--- controller/statistic.js
+++ controller/statistic.js
@@ -15,11 +15,39 @@ export const countWordInHistoryEngVie = (req, res) => {
     });
 }
 
+export const countWordInHistoryVieEng = (req, res) => {
+    const {id} = req.params;
+    sql.connect(config).then(pool => {
+        return pool.request()
+            .query(`select count(idDicEV) as 'count' from HistoryWord_Vie_Eng where idUser = ${id}`);
+        
+    }).then(result => {
+        res.send(result.recordset);
+        
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
 export const countWordInYourWordEngVie = (req, res) => {
     const {id} = req.params;
     sql.connect(config).then(pool => {
         return pool.request()
             .query(`select count(idDicEV) as 'count' from YourWords_Eng_Vie where idUser = ${id}`);
+        
+    }).then(result => {
+        res.send(result.recordset);
+        
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+export const countWordInYourWordVieEng = (req, res) => {
+    const {id} = req.params;
+    sql.connect(config).then(pool => {
+        return pool.request()
+            .query(`select count(idDicEV) as 'count' from YourWords_Vie_Eng where idUser = ${id}`);
         
     }).then(result => {
         res.send(result.recordset);
--- routes/statistics.js
+++ routes/statistics.js
@@ -1,12 +1,16 @@
 
 import express from "express";
 
-import { countWordInHistoryEngVie, countWordInYourWordEngVie } from "../controller/statistic.js";
+import { countWordInHistoryEngVie, countWordInYourWordEngVie, countWordInHistoryVieEng, countWordInYourWordVieEng } from "../controller/statistic.js";
 
 const router = express.Router();
 
 router.get('/countHistoryWordEngVie/:id', countWordInHistoryEngVie);
 
+router.get('/countHistoryWordVieEng/:id', countWordInHistoryVieEng);
+
 router.get('/countYourWordEngVie/:id', countWordInYourWordEngVie);
+
+router.get('/countYourWordVieEng/:id', countWordInYourWordVieEng);
 
 export default router;
